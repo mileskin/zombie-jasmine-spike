8aa9035acbf2ada3f7cd52c61ac90ef438bc269b
Miki Leskinen <mileskin@w154.wlan.reaktor.fi>
2011-01-12 11:47:58 +0200

    Toggle trends using link.

--- application/public/js/application.js
+++ application/public/js/application.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $.ajax({
-    url: 'http://api.twitter.com/1/trends/current.json',
-    type: 'GET',
-    dataType: 'jsonp',
-    success: function(data) {
-      var trends = _(data.trends).chain().map(function(v, k){ return v }).first().flatten().value()
-      _.forEach(trends, function(t) {
-        $('.trends').append('<li class="trend">' + t.name + '</li>')
+  $('.actions .showTrends').live('click', function(event) {
+    event.preventDefault()
+    if ($('.trends .trend').length === 0) {
+      $.ajax({
+        url: 'http://api.twitter.com/1/trends/current.json',
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(data) {
+          var trends = _(data.trends).chain().map(function(v, k){ return v }).first().flatten().value()
+          _.forEach(trends, function(t) {
+            $('.trends').append('<li class="trend">' + t.name + '</li>')
+          })
+        }
       })
+    } else {
+      $('.trends').empty()
     }
   })
 })
--- spec/js/acceptance/foo-twitter-client-spec.js
+++ spec/js/acceptance/foo-twitter-client-spec.js
@@ -11,10 +11,26 @@ describe('twitter client', function() {
       asyncSpecWait()
     })
 
-    it('shows latest trends', function() {
+    it('has trends link', function() {
       browser.visit('http://localhost:8003/', function(error, browser) {
-        expect(browser.querySelectorAll('.trends .trend').length).toEqual(10)
+        expect(browser.text('.actions .showTrends')).toEqual('Trends')
         asyncSpecDone()
+      })
+      asyncSpecWait()
+    })
+  })
+
+  describe('when toggling trends link', function() {
+    it('shows and hides latest trends', function() {
+      expect(browser.querySelectorAll('.trends .trend').length).toEqual(0)
+      browser.visit('http://localhost:8003/', function(error, browser) {
+        browser.clickLink('.showTrends', function(error, browser) {
+          expect(browser.querySelectorAll('.trends .trend').length).toEqual(10)
+          browser.clickLink('.showTrends', function(error, browser) {
+            expect(browser.querySelectorAll('.trends .trend').length).toEqual(0)
+            asyncSpecDone()
+          })
+        })
       })
       asyncSpecWait()
     })
