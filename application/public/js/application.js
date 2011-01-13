51db1ca9c67c39e4e71bf52cc233d0b4ad19d633
Miki Leskinen <miki.leskinen@reaktor.fi>
2011-01-13 16:04:45 +0200

    Introduce namespace.

--- application/public/js/application.js
+++ application/public/js/application.js
@@ -1,20 +1,34 @@
+$.fooTwitterClient = (function($) {
+  function initTrendsBehavior() {
+    $('.actions .showTrends').live('click', function(event) {
+      event.preventDefault()
+      if ($('.trends .trend').length === 0) {
+        $.ajax({
+          url: 'http://api.twitter.com/1/trends/current.json',
+          type: 'GET',
+          dataType: 'jsonp',
+          success: function(data) {
+            var trends = _(data.trends).chain().map(function(v, k){ return v }).first().flatten().value()
+            _.forEach(trends, function(t) {
+              $('.trends').append('<li class="trend">' + t.name + '</li>')
+            })
+          }
+        })
+      } else {
+        $('.trends').empty()
+      }
+    })
+  }
+
+  function initBehavior() {
+    initTrendsBehavior()
+  }
+
+  return {
+    initBehavior: initBehavior
+  }
+}(jQuery))
+
 $(document).ready(function() {
-  $('.actions .showTrends').live('click', function(event) {
-    event.preventDefault()
-    if ($('.trends .trend').length === 0) {
-      $.ajax({
-        url: 'http://api.twitter.com/1/trends/current.json',
-        type: 'GET',
-        dataType: 'jsonp',
-        success: function(data) {
-          var trends = _(data.trends).chain().map(function(v, k){ return v }).first().flatten().value()
-          _.forEach(trends, function(t) {
-            $('.trends').append('<li class="trend">' + t.name + '</li>')
-          })
-        }
-      })
-    } else {
-      $('.trends').empty()
-    }
-  })
+  $.fooTwitterClient.initBehavior()
 })
