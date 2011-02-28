fb3fca80ec02d7ac39f455ee08b334f05fa5ab9c
Miki Leskinen <miki.leskinen@reaktor.fi>
2011-02-28 21:45:06 +0200

    Extract private .showLatestTrends.

--- application/static/js/trends.js
+++ application/static/js/trends.js
@@ -3,22 +3,26 @@
     $('.actions .showTrends').live('click', function(event) {
       event.preventDefault()
       if ($('.trends .trend').length === 0) {
-        $.ajax({
-          url: 'http://api.twitter.com/1/trends/current.json',
-          type: 'GET',
-          dataType: 'jsonp',
-          success: function(data) {
-            var trends = _(data.trends).chain().map(function(v, k){ return v }).first().flatten().value()
-            _.forEach(trends, function(t) {
-              $('.trends').append('<li class="trend">' + t.name + '</li>')
-            })
-          },
-          error: function(xhr) {
-            $('.feedback').text(xhr.responseText)
-          }
-        })
+        showLatestTrends()
       } else {
         $('.trends').empty()
+      }
+    })
+  }
+
+  function showLatestTrends() {
+    $.ajax({
+      url: 'http://api.twitter.com/1/trends/current.json',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data) {
+        var trends = _(data.trends).chain().map(function(v, k){ return v }).first().flatten().value()
+        _.forEach(trends, function(t) {
+          $('.trends').append('<li class="trend">' + t.name + '</li>')
+        })
+      },
+      error: function(xhr) {
+        $('.feedback').text(xhr.responseText)
       }
     })
   }
